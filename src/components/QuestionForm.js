27b63e0fd2316495b1da928cd6ce83bec4655780
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-28 19:35:02 +0300

    - done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ setQuestions }) {
+
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -20,6 +21,23 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      headers: {"Content-Type": "application/json"},
+      method: "POST",
+      body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(data => {
+      setFormData({
+        prompt: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        correctIndex: 0,
+      })
+      setQuestions(prev => [...prev, data])
+    })
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,23 @@
 import React from "react";
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  function handleDelete(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    }).then(()=>setQuestions((previousData)=>previousData.filter(question =>  question.id !== id)))
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+        { questions.map((question) => {
+          return <QuestionItem question= {question} key={question.id} handleDelete={()=> handleDelete(question.id)}/>
+          }) }
+      </ul>
     </section>
   );
 }
-
 export default QuestionList;
